ea46a1e4f73c275f3d1398dfcd5f53b2cc482754
r8025n <raihanshovon95@gmail.com>
2025-06-12 14:59:25 +0600

    Implement feed forward function

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from network import Network
 def main():
     nn = Network([2, 3, 1])
     print(nn.weights)
+    nn.feed_forward([1, 2])
 
 if __name__ == "__main__":
     main()
+    
--- network.py
+++ network.py
@@ -1,9 +1,23 @@
 import numpy as np
 
+def sigmoid(z):
+    return 1.0/(1.0 + np.exp(-z))
+
 class Network:
     def __init__(self, sizes):
         num_layers: len(sizes)
         self.weights = [np.random.randn(x, y) for x, y in zip(sizes[1:], sizes[:-1])]
-        self.bias = [np.random.randn(x, 1) for x in sizes[1:]]
+        self.biases = [np.random.randn(x, 1) for x in sizes[1:]]
 
+    def feed_forward(self, input):
+        input = np.array(input)
+        
+        for w, b in zip(self.weights, self.biases):
+            print("w shape:", w.shape)
+            print("input shape:", input.shape)
+            input = sigmoid(np.dot(w, input))
+            print("w.b dot product shape:", input.shape)
+            print("----------------------")
+        
+        return input
     
